7837026ed3bdf07f73865211e86b3a25dea30dbb
lazyluis <ping@side.one>
2025-06-19 10:26:30 +0800

    add ibc implementation

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@cosmos/solidity-ibc-eureka/src/v1/ERC20IBCEureka.sol";
+import "./IMintableAndBurnable.sol";
 
-contract MyToken is ERC20, ERC20Permit {
+contract MyToken is ERC20, ERC20Permit, IMintableAndBurnable {
     constructor() ERC20("MyToken2", "MTK") ERC20Permit("MyToken2") {}
+    function mint(address mintAddress, uint256 amount) external {}
+
+    function burn(address mintAddress, uint256 amount) external {}
+
 }
